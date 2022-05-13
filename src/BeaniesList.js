d5133575804d27a51eaf19fbedda75c02a4e3b10
Khayman King <khaymanaking@gmail.com>
2022-05-12 22:29:51 -0700

    mapping works in class

--- src/BeaniesList.js
+++ src/BeaniesList.js
@@ -1,12 +1,14 @@
 import BeanieBaby from './BeanieBaby.js';
-
+import React from 'react';
 //TODO add a load spinner
-export default function BeaniesList({ beanieBabies }) {
-  return (
-    <div className='beanies'>
-      {
-        beanieBabies.map(baby => <BeanieBaby key={baby.id} {...baby} />)
-      }
-      {/* render out the beanie babies as a list */}
-    </div>);
+export default class BeaniesList extends React.Component {
+  render(){
+    return (
+      <div className='beanies'>
+        {
+          this.props.beanieBabies.map(baby => <BeanieBaby key={baby.id} {...baby} />)
+        }
+        {/* render out the beanie babies as a list */}
+      </div>);
+  }
 }
